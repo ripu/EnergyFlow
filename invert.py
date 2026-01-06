42b5bbd52029f3410a1522fd10b016bdce9cb4e5
ripu <6645111+ripu@users.noreply.github.com>
2026-01-06 23:01:15 +0100

    fix: disable server-side caching to prevent stale data

--- invert.py
+++ invert.py
@@ -244,6 +244,10 @@ def make_handler(count: int):
                 self.send_response(200)
                 self.send_header("Content-Type", mime)
                 self.send_header("Content-Length", str(len(content)))
+                # Disable Caching
+                self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
+                self.send_header("Pragma", "no-cache")
+                self.send_header("Expires", "0")
                 self.end_headers()
                 self.wfile.write(content)
             except Exception as e:
